820af34e625384f423af0b39ea95b468c28253b5
Massawe14 <ramadhanimassawe14@gmail.com>
2024-02-24 23:13:45 +0300

    Update and configure firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 
 // Entry point of Flutter App
-void main(){
+void main() async {
+  // Initialize Firebase
+  await Firebase.initializeApp( options: DefaultFirebaseOptions.currentPlatform).then(
+    (FirebaseApp value) => Get.put(AuthenticationRepository())
+  );
   runApp(
     const App(),
   );
